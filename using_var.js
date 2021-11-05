013d0ce99b254d8b6c726224067033d85df961f9
Tatyana Asenova <77272421+Tatyana-OA@users.noreply.github.com>
2021-11-05 11:02:19 +0200

    Update using_var.js

--- using_var.js
+++ using_var.js
@@ -1,8 +1,17 @@
 var sayHello = 'Say Hello';
 
 function hello  () {
-	var sayHi = 'Say Hi';
+	var sayHi = 'Say Hi'; // available anywhere in the function 
+
+	if (sayHi === 'Say Hi') {
+		let sayBye = 'Say Bye';
+		console.log(sayBye) // will return value -> available anywhere in the block it is defined in
+	}
+
+	console.log(sayBye) // will not  return value  (function scope)
 
 }
-console.log(sayHello) //will return value
-console.log(sayHi) //will be undefined
+hello()
+console.log(sayHello) // will return value
+console.log(sayHi) // will be undefined
+
